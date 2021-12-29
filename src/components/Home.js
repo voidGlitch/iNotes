5de899a0a42ca52891a848795e6911f9298a2c94
voidGlitch <official.shreshthav@gmail.com>
2021-12-29 12:03:21 +0530

    added navbar and routes to the inotes

---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+export const Home = () => {
+  return <div>This is home</div>;
+};
+
+export default Home;
